3507b747d7a1871e04838324af834cc95052eceb
User <ug-fca-gig@ug.edu.ec>
2025-05-08 11:57:38 -0500

    figurasgeometricas,color

---
+++ cuadrado.py
@@ -0,0 +1,19 @@
+from rectangulo import FiguraGeometrica
+class Cuadrado(FiguraGeometrica):
+    def __init__(self, lado):
+        self.lado = lado
+
+    def area(self):
+        return self.lado ** 2
+
+    def perimetro(self):
+        return 4 * self.lado
+
+
+# Ejemplo de uso
+cuadrado = Cuadrado(8)
+print("Área del cuadrado:", cuadrado.area())
+print("Perímetro del cuadrado:", cuadrado.perimetro())
+
+
+
